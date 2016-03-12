5f6d24a8cbee3c65efdf1aba814a8d8026aa2061
Kleber Shimabuku <klebershimabuku@gmail.com>
2016-03-12 10:09:01 -0300

    Implement delete function on bucket

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -12,4 +12,15 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "deletes value by key and returns current values", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+    KV.Bucket.put(bucket, "eggs", 5)
+
+    assert KV.Bucket.delete(bucket, "milk") == 3
+    assert KV.Bucket.delete(bucket, "milk") == nil
+
+    assert KV.Bucket.get(bucket, "eggs") == 5
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
